1ccd78e77b43f44271f3024e4f60cf478c4b05c1
Alexander Krapukhin <alex.krapukhin@nami.ru>
2020-01-29 20:00:23 +0300

    add comments, rename variables, add version

--- jobfinder.py
+++ jobfinder.py
@@ -1,31 +1,24 @@
 import requests
 import codecs
-x=[]
-url = 'https://api.hh.ru/vacancies'
+version = "29012020a"
 
+# read query from file
 file_query = codecs.open('query.txt', 'r', 'utf-8')
 query = file_query.readlines()
-print(query)
-print(len(query))
-query_words = []
-query_words_strip = []
 query_string = ""
 for w in query:
-	query_words.append(w)
-	query_words_strip.append(w.strip())
 	query_string += w.strip() + " "
-print(query_words)
-print(query_words_strip)
-print(query_string.strip())
+query_string = query_string.strip()
 
+# read included areas
 file_areas = codecs.open('include_areas.txt', 'r', 'utf-8')
 areas = file_areas.readlines()
 areas_list = []
 for area in areas:
-	if len(area) > 1:
+	area = area.strip().lower()
+	if area != '':
 		areas_list.append(area)
-print(areas_list)
-areas_ids = []
+
 russia_areas = []
 all_areas = requests.get('https://api.hh.ru/areas')
 all_areas = all_areas.json()
@@ -34,31 +27,35 @@ for area in all_areas:
 		russia_id = area['id']
 		russia_areas = area['areas']
 			
-			
+# read excluded areas
 file_excl_areas = codecs.open('exclude_areas.txt', 'r', 'utf-8')
 excluded_areas = file_excl_areas.readlines()
-print(excluded_areas)
 excluded_areas_noendls = []
 for area in excluded_areas:
-	if area[-1] == '\n':
-		area = area[:-1]
-	excluded_areas_noendls.append(area.lower())
-print("excluded_areas_noendls:")
-print(excluded_areas_noendls)		
-		
-#print(russia_areas)
+	area = area.strip().lower()
+	if area != '':
+		excluded_areas_noendls.append(area)	
+
+# load list of companies
+companies = []
+import urllib.request  # the lib that handles the url stuff
+for line in urllib.request.urlopen('https://raw.githubusercontent.com/akrapukhin/jobfinder/master/companies.txt'):
+	companies.append(line.decode('utf-8').strip())
+companies = set(companies)
+companies = list(companies)
+companies_nums = []
+for s in companies:
+    companies_nums.append(s[23:])
+	
+# find corresponding areas IDs
+areas_ids = []
 if (len(areas_list) == 0):
 	areas_ids.append(russia_id)
+	areas_list.append("Россия")
 else:
 	for area in areas_list:
-		if area[-1] == '\n':
-			area = area[:-1]
 		area_found = False
-		print('area: ' + area)
 		for region in russia_areas:
-			print(region['name'].lower())
-			print(area.lower())
-			print(region['name'].lower() == area.lower())
 			if region['name'].lower() == area.lower():
 				areas_ids.append(region['id'])
 				area_found = True
@@ -68,71 +65,46 @@ else:
 					area_found = True
 		if not area_found:
 			print('территория не найдена: ' + area)
-			
-print('areas_ids: ' + str(areas_ids))
-#testtxt = requests.get('https://raw.githubusercontent.com/akrapukhin/jobfinder/master/companies.txt')
-#test_test = testtxt.content
-#for s in test_test:
-#	print(s)
 
-data = []
-import urllib.request  # the lib that handles the url stuff
-for line in urllib.request.urlopen('https://raw.githubusercontent.com/akrapukhin/jobfinder/master/companies.txt'):
-	data.append(line.decode('utf-8'))
-    #print(line.decode('utf-8')) #utf-8 or iso8859-1 or whatever the page encoding scheme is
-
-#print(data)
-file_companies = open("companies.txt", "r")
-companies = file_companies.readlines()
-#companies = data
-companies_set = set(companies)
-companies_list = list(companies_set)
-companies_nums = []
-for s in companies_list:
-    companies_nums.append(s[23:-1])
-
-#print(companies_nums)
+# parameters of query to hh
 par = {'text': query_string,
-	   'area': [1, 2],
+	   'area': areas_ids,
        'per_page': '100',
        'employer_id' : companies_nums,
        'order_by' : 'publication_time',
        'page': 0}
        
-#par = {'text': 'с++',
-#      'area': areas_ids,
-#       'per_page': '100',
-#       'employer_id' : companies_nums,
-#       'order_by' : 'publication_time',
-#       'page': 0}
-r = requests.get(url, params=par)
-e=r.json()
-x.append(e)
-#print(e)
-num_of_vacancies = e['found']
-num_of_pages = e['pages']
+# request to hh to get all vacancies
+all_pages = []
+first_page_vacancies = requests.get('https://api.hh.ru/vacancies', params=par)
+first_page_vacancies = first_page_vacancies.json()
+all_pages.append(first_page_vacancies)
+
+num_of_vacancies = first_page_vacancies['found']
+num_of_pages = first_page_vacancies['pages']
 print("num_of_pages: " + str(num_of_pages))
+print("num_of_vacancies: " + str(num_of_vacancies))
 
 for pagenum in range(num_of_pages-1):
     par['page'] = pagenum+1
-    r = requests.get(url, params=par)
-    e = r.json()
-    x.append(e)
+    page_vacancies = requests.get('https://api.hh.ru/vacancies', params=par)
+    page_vacancies = page_vacancies.json()
+    all_pages.append(page_vacancies)
 
 #print(e['alternate_url'])
 html = ""
 
 counter = 1
-for j in x:
-	y = j['items']
-	for i in y:
+for page in all_pages:
+	vacancies = page['items']
+	for vac in vacancies:
 		excluded = False
 		for excluded_area in excluded_areas_noendls:
-			if excluded_area.lower() == i['area']['name'].lower():
+			if excluded_area.lower() == vac['area']['name'].lower():
 				excluded = True;
 		if (not excluded):	
-			#print (i['area']['name'].lower() + " not in " + str(excluded_areas_noendls))
-			salary = i['salary']
+			# get salary
+			salary = vac['salary']
 			salary_str = ""
 			if salary == None:
 				salary_str = "не указана"
@@ -145,17 +117,27 @@ for j in x:
 					salary_str += " до вычета"
 				else:
 					salary_str += " на руки"
+			# get logo
 			logo_str = ""
-			if i['employer']['logo_urls'] != None:
-				if i['employer']['logo_urls']['90'] != None:
-					logo_str = i['employer']['logo_urls']['90']
-
-			html_line = "<tr><td>"
-			html_line += str(counter) + "</td><td>" + i['name'] + "</td><td>" + i['employer']['name'] + "</td> <td align=\"center\">" + "<img src=\"" + logo_str + "\"" + "></td> <td>" + i['area']['name'] + "</td> <td>" + salary_str +"</td> <td style=\"white-space:nowrap;\">"+ i['published_at'][8:10] + i['published_at'][7] + i['published_at'][5:7] + i['published_at'][4] + i['published_at'][0:4] +  "</td> "
-			html_line += "<td><a href = \"" + i['alternate_url'] + "\" target=\"_blank\">" + i['alternate_url'] + "</a></td>"
-			#html_line += i['published_at'][:10] + " " + i['alternate_url']
-			html_line += "</tr>"
-			#print(i['name'] + " " + i['employer']['name'] + " " + i['published_at'][:10] + " " + i['alternate_url'])
+			if vac['employer']['logo_urls'] != None:
+				if vac['employer']['logo_urls']['90'] != None:
+					logo_str = vac['employer']['logo_urls']['90']
+			
+			# generate one line in HTML file with vacancy info
+			html_line = "<tr><td>" + str(counter) #number
+			html_line += "</td><td>" + vac['name'] #vacancy title
+			html_line += "</td><td>" + vac['employer']['name'] #employer name
+			html_line += "</td><td align=\"center\">" + "<img src=\"" + logo_str + "\"" + ">" #employer logo
+			html_line += "</td><td>" + vac['area']['name'] #vacancy area
+			html_line += "</td><td>" + salary_str #salary
+			html_line += "</td><td style=\"white-space:nowrap;\">"+ vac['published_at'][8:10]\
+			                                                      + vac['published_at'][7]\
+			                                                      + vac['published_at'][5:7]\
+			                                                      + vac['published_at'][4]\
+			                                                      + vac['published_at'][0:4] + " "\
+			                                                      + vac['published_at'][11:16] #time of publication
+			html_line += "</td><td><a href = \"" + vac['alternate_url'] + "\" target=\"_blank\">" + vac['alternate_url'] + "</a></td></tr>" #link to vacancy
+			#print(vac['name'] + " " + vac['employer']['name'] + " " + vac['published_at'] + " " + vac['alternate_url'])
 			html += html_line
 			counter += 1
 		else:
@@ -163,36 +145,18 @@ for j in x:
 
 print('num of vacancies: ' + str(num_of_vacancies))
 html += "</body></html>"
-
-html_start = """<!DOCTYPE html><html><head><style>table, th, td {border: 1px solid black;}</style><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body><h1>"""
-html_start+= str(num_of_vacancies) + " вакансий найдено:" + "</h1>"
-
-html_start+= "<table style=\"width:100%\"><tr><th>#</th><th>Должность</th><th>Компания</th><th>Лого</th><th>Город</th><th>Зарплата</th><th>Дата</th><th>Ссылка</th></tr>"
+html_start = """<!DOCTYPE html><html><head><style>table, th, td {border: 1px solid black;}</style><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head><body>"""
+html_start+= "<t>" + "<b>" +"вакансий найдено: "+ "</b>"  + str(num_of_vacancies) + "</t><br/>"
+html_start+= "<t>" + "<b>" +"область поиска: "+ "</b>"  + str(areas_list) + "</t><br/>"
+html_start+= "<t>" + "<b>" +"исключения: "+ "</b>"  + str(excluded_areas_noendls) + "</t><br/>"
+html_start+= "<t>" + "<b>" + "запрос: "+ "</b>"  + query_string + "</t><br/>"
+html_start+= "<t>" + "<b>" + "кол-во компаний из списка на hh: " + "</b>" + str(len(companies_nums)) + "</t><br/>"
+html_start+= "<table style=\"width:100%\"><tr><th>#</th><th>Должность</th><th>Компания</th><th>Лого</th><th>Город</th><th>Зарплата</th><th>Дата и время (мск)</th><th>Ссылка</th></tr>"
 html = html_start + html
-#параметры, которые будут добавлены к запросу
-# file_area = open("area.txt","r")
-# area = file_area.readline()
-# if lower(area) == 'россия':
-#     countries_result = requests.get('https://api.hh.ru/areas/countries')
-#     countries = countries_result.json()
-#     for country in countries:
-#         if lower(country['name']) == 'россия':
-#             area_id = country['id']
 
-# regions = requests.get('https://api.hh.ru/areas/countries')
-# reg=regions.json()
-# print(reg)
-
-#file_results = open("results.html", "w+", encoding="utf-8")
 file_results = codecs.open('results_test_table.html', 'w', 'utf-8')
 file_results.write(html)
 file_results.close()
 
 import webbrowser
-url = 'results_test_table.html'
-webbrowser.open(url, new=2)  # open in new tab
-
-test_list = ['москва', 'сочи', 'санкт-петербург']
-print(test_list)
-test_boo = 'санкт-петербург' not in test_list
-print(test_boo)
+webbrowser.open('results_test_table.html', new=2)  # open in new tab
